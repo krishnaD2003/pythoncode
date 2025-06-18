607d4a43f84c2c165b57c02fc40fc56b2634c249
krishna <krishnadudhambe@gmail.com>
2025-06-18 14:27:20 +0530

    Create k_letter_using_matplotlib.py

---
+++ k_letter_using_matplotlib.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+a = np.array([[0,1,0,0,0,1],[0,1,0,0,1,0],[0,1,0,1,0,0],[0,1,1,0,0,0],[0,1,1,0,0,0],[0,1,0,1,0,0],[0,1,0,0,1,0],[0,1,0,0,0,1]])
+a
+
+plt.matshow(a)
+plt.gray()
